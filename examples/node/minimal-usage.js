27eca3cbeeb68fdb2646b93e024ce8faab56803f
karthikbhagi2727 <16j41a0211@gmail.com>
2020-05-03 18:52:39 +0530

    output commit

--- examples/node/minimal-usage.js
+++ examples/node/minimal-usage.js
@@ -3,15 +3,15 @@
 var faker = require('../../index');
 faker.locale = "en";
 
-//console.log(faker.lorem.sentences())
+console.log(faker.lorem.sentences())
 
 console.log(faker.name.findName())
-return;
-//console.log(faker.address)
+
+console.log(faker.address)
 console.log(faker.internet.email())
 console.log(faker.date.recent())
 console.log(faker.helpers.contextualCard());
-
+console.log(faker.laptop.laptop());
+return;
 faker.locale = "uk";
-
 console.log(faker.helpers.contextualCard());
--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 var Faker = require('./lib');
 var faker = new Faker({ locales: require('./lib/locales') });
 module['exports'] = faker; 
-
+console.log(faker.laptop.laptop());
 
 
